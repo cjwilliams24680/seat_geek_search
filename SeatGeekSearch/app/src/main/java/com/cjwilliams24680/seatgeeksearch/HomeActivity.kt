843e46db6d171b597cf6fc23f4f2c5e6a196b77e
Chris <chris.williams@gametime.co>
2018-04-10 09:18:26 -0700

    Created empty project

---
+++ SeatGeekSearch/app/src/main/java/com/cjwilliams24680/seatgeeksearch/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.cjwilliams24680.seatgeeksearch
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
